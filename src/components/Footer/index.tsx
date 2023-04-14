2a77621437a129918f1a639a48c94d8534101401
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-04-14 10:40:02 -0300

    feat: project done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.css'
 const Footer = () => (
   <footer className={styles.footer}>
     <p>
-      Made with TS + React by
+      Developed by
       <span><a href="https://github.com/devgustavomacedo" target='_blank' rel="noreferrer">@GustavoMacedo</a></span>
     </p>
   </footer>
